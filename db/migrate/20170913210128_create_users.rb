3c90227f4c48ae163ca08a3742c6cdc50cf781f4
Alexander Calleia <alex_calleia@yahoo.com>
2017-09-13 17:15:50 -0400

    add last_name column

--- db/migrate/20170913210128_create_users.rb
+++ db/migrate/20170913210128_create_users.rb
@@ -4,7 +4,8 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.string :username, index: true
       t.string :password_digest
       t.string :email
-      t.string :name
+      t.string :first_name
+      t.string :last_name
       t.string :auth_token
 
       t.timestamps
