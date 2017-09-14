abe6ee9e7967105427e9ff8be6e672d991f29aff
Alexander Calleia <alex_calleia@yahoo.com>
2017-09-13 20:46:35 -0400

    add artworks to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170913210128) do
+ActiveRecord::Schema.define(version: 20170914001004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artworks", force: :cascade do |t|
+    t.string "url"
+    t.string "title"
+    t.string "description"
+    t.string "date"
+    t.string "prompt"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_artworks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
