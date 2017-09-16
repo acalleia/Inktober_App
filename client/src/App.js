8cb21756cfe8a8d061875565d8d15310b479b132
Alexander Calleia <alex_calleia@yahoo.com>
2017-09-16 18:16:08 -0400

    add routes to user components

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,20 @@ import { base } from './base';
 import Message from './components/Message';
 import firebase from 'firebase';
 import FileUploader from 'react-firebase-file-uploader';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import axios from 'axios'; 
+
+import Auth from './modules/Auth';
+import Nav from './components/Nav';
+import LoginForm from './components/LoginForm';
+import RegisterForm from './components/RegisterForm';
+import Dashboard from './components/Dashboard';
+import Home from './components/Home';
 
 class App extends Component {
   constructor(props) {
@@ -16,13 +30,27 @@ class App extends Component {
      avatar: '',
      isUploading: false,
      progress: 0,
-     avatarURL: ''
+     avatarURL: '',
+     loginUserName: '',
+     loginPassword: '',
+     registerUserName: '',
+     registerPassword: '',
+     registerEmail: '',
+     registerName: '',
     };
 
     this.rootRef = firebase.database().ref();
     this.messageRef = this.rootRef.child('messages');
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
+    this.logoutUser = this.logoutUser.bind(this);
+    this.handleRegisterSubmit = this.handleRegisterSubmit.bind(this);
+
+
+    this.resetFireRedirect = this.resetFireRedirect.bind(this);
   }
 
  
@@ -79,6 +107,81 @@ class App extends Component {
   }
 
 
+  // Auth
+  handleInputChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleLoginSubmit(e) {
+    e.preventDefault();
+    axios.post('/login', {
+      username: this.state.loginUserName,
+      password: this.state.loginPassword,
+    }).then(res => {
+      console.log(res);
+      if (res.data.token) {
+        Auth.authenticateToken(res.data.token);
+        this.setState({
+          auth: Auth.isUserAuthenticated(),
+          loginUserName: '',
+          loginUserPassword: '',
+        })
+      }
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+
+  handleRegisterSubmit(e) {
+    e.preventDefault();
+    axios.post('/users', {
+      user: {
+        username: this.state.registerUserName,
+        password: this.state.registerPassword,
+        email: this.state.registerEmail,
+        name: this.state.registerName,
+      }
+    }).then(res => {
+      if (res.data.token) {
+        Auth.authenticateToken(res.data.token);
+        this.setState({
+          auth: Auth.isUserAuthenticated(),
+        })
+      }
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+
+  resetFireRedirect() {
+    if (this.state.shouldFireRedirect) {
+      this.setState({
+        shouldFireRedirect: false,
+      })
+    }
+  }
+
+  logoutUser() {
+    axios.delete('/logout', {
+      headers: {
+        'Authorization': `Token ${Auth.getToken()}`,
+        token: Auth.getToken(),
+      }
+    }).then(res => {
+      Auth.deauthenticateUser();
+      this.setState({
+        auth: Auth.isUserAuthenticated(),
+        loginUserName: '',
+        loginUserPassword: '',
+      })
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -116,6 +219,56 @@ class App extends Component {
             onProgress={this.handleProgress}
           />
         </form>
+        <Router>
+        <div className="app">
+          <Nav logoutUser={this.logoutUser} />
+          <Route exact path="/" component={Home} />
+          <Route
+            exact
+            path="/login"
+            render={() =>
+              !this.state.auth ? (
+                <LoginForm
+                  auth={this.state.auth}
+                  loginUserName={this.state.loginUserName}
+                  loginPassword={this.state.loginPassword}
+                  handleInputChange={this.handleInputChange}
+                  handleLoginSubmit={this.handleLoginSubmit}
+                />
+              ) : (
+                <Redirect to="/dashboard" />
+              )}
+          />
+          <Route
+            exact
+            path="/register"
+            render={() => 
+              !this.state.auth ? (
+                <RegisterForm
+                  auth={this.state.auth}
+                  registerUserName={this.state.registerUserName}
+                  registerPassword={this.state.registerPassword}
+                  registerEmail={this.state.registerEmail}
+                  registerFirstName={this.state.registerFirstName}
+                  registerLastName={this.state.registerLastName}
+                  handleInputChange={this.handleInputChange}
+                  handleRegisterSubmit={this.handleRegisterSubmit}
+                />
+              ) : (
+                <Redirect to="/dashboard" />
+              )}
+            />
+          <Route
+              exact
+              path="/dashboard"
+              render={() =>
+                this.state.auth ? <Dashboard auth={this.state.auth} resetFireRedirect={this.resetFireRedirect} />
+                : <Redirect to="/login" />}
+          />
+          
+          </div>
+          </Router>
+
     </div>
     );
   }
