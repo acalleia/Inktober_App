8ca871ef2ebdf5dfbf1b0f30bf9eda182076b571
Alexander Calleia <alex_calleia@yahoo.com>
2017-09-17 16:33:25 -0400

    add user_artworks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApiController
 
   def profile
     user = User.find_by_auth_token!(request.headers[:token])
+    user_artworks = Artwork.where(user_id: user.id)
     render json: { 
       user: { username: user.username, email: user.email, first_name: user.first_name, last_name: user.last_name },
       artworks: user_artworks,
