2cd073265a93c184412489b9ef620559898936ff
Alexander Calleia <alex_calleia@yahoo.com>
2017-09-20 02:05:01 -0400

    add artwork model relations to favorites and comments

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,3 +1,8 @@
 class Artwork < ApplicationRecord
+  
   belongs_to :user
+  has_many :favorites
+  has_many :users, through: :favorites
+  has_many :comments
+  has_many :users, through: :comments
 end
