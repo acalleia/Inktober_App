f9aaffc3d2cf53302dd1ebc476f31457eb7a8b63
Alexander Calleia <alex_calleia@yahoo.com>
2017-09-20 02:12:39 -0400

    turn image into button to comments page view

--- client/src/components/ArtworkSingle.jsx
+++ client/src/components/ArtworkSingle.jsx
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 class Artwork extends Component{
   constructor(){
     super();
+
+  }
+
+  componentDidMount(){
+    console.log(this.props.artwork.id)
   }
 
   render() {
   return (
       <div className="ind-artwork">
-        <img src={this.props.artwork.url} alt={this.props.artwork.title} />
-        <h3>{this.props.artwork.title}</h3>
-        <p>{this.props.artwork.prompt}</p>
-        <p>{this.props.artwork.desciption}</p>
-        {(this.props.artwork.username) ? <p className="user">{this.props.artwork.username}</p> : '' }
+        <Link to={`/artworks/${this.props.artwork.id}/comments`}>
+          <img 
+           onClick={() => 
+           this.props.handleComments(this.props.artwork.id)}
+          src={this.props.artwork.url} alt={this.props.artwork.title}
+          alt={this.props.title} value={this.props.artwork.id}
+          />
+          </Link>
+          <h3>{this.props.artwork.title}</h3>
+          <p>Prompt: {this.props.artwork.prompt}</p>
+          <p>Desciption: {this.props.artwork.description}</p>
+          {(this.props.artwork.username) ? <p className="user">{this.props.artwork.username}</p> : '' } 
       </div>
     );
   }
