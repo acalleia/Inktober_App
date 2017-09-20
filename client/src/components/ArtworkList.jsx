9f4634dd89243eba6af8cd57a1a328474ab541f1
Alexander Calleia <alex_calleia@yahoo.com>
2017-09-20 02:10:37 -0400

    add classnames for css

--- client/src/components/ArtworkList.jsx
+++ client/src/components/ArtworkList.jsx
@@ -35,39 +35,40 @@ class ArtworkList extends Component {
           name="artworkPrompt"
           value={this.props.artworkPrompt}
           onChange={this.props.handleInputChange}
+          className="dropdown-select"
         >
-          <option defaultValue="" value="">All Images</option>
-          <option value="Swift">Swift</option>
-          <option value="Divided">Divided</option>
-          <option value="Poison">Poison</option>
-          <option value="Underwater">Underwater</option>
-          <option value="Long">Long</option>
-          <option value="Sword">Sword</option>
-          <option value="Shy">Shy</option>
-          <option value="Crooked">Crooked</option>
-          <option value="Screech">Screech</option>
-          <option value="Gigantic">Gigantic</option>
-          <option value="Run">Run</option>
-          <option value="Shattered">Shattered</option>
-          <option value="Teeming">Teeming</option>
-          <option value="Fierce">Fierce</option>
-          <option value="Mysterious">Mysterious</option>
-          <option value="Fat">Fat</option>
-          <option value="Graceful">Graceful</option>
-          <option value="Filthy">Filthy</option>
-          <option value="Cloud">Cloud</option>
-          <option value="Deep">Deep</option>
-          <option value="Furious">Furious</option>
-          <option value="Trail">Trail</option>
-          <option value="Juicy">Juicy</option>
-          <option value="Blind">Blind</option>
-          <option value="Ship">Ship</option>
-          <option value="Squeak">Squeak</option>
-          <option value="Climb">Climb</option>
-          <option value="Fall">Fall</option>
-          <option value="United">United</option>
-          <option value="Found">Found</option>
-          <option value="Mask">Mask</option>
+          <option className="dropdown-options" defaultValue="" value="">All Images</option>
+          <option className="dropdown-options" value="Swift">Swift</option>
+          <option className="dropdown-options" value="Divided">Divided</option>
+          <option className="dropdown-options" value="Poison">Poison</option>
+          <option className="dropdown-options" value="Underwater">Underwater</option>
+          <option className="dropdown-options" value="Long">Long</option>
+          <option className="dropdown-options" value="Sword">Sword</option>
+          <option className="dropdown-options" value="Shy">Shy</option>
+          <option className="dropdown-options" value="Crooked">Crooked</option>
+          <option className="dropdown-options" value="Screech">Screech</option>
+          <option className="dropdown-options" value="Gigantic">Gigantic</option>
+          <option className="dropdown-options" value="Run">Run</option>
+          <option className="dropdown-options" value="Shattered">Shattered</option>
+          <option className="dropdown-options" value="Teeming">Teeming</option>
+          <option className="dropdown-options" value="Fierce">Fierce</option>
+          <option className="dropdown-options" value="Mysterious">Mysterious</option>
+          <option className="dropdown-options" value="Fat">Fat</option>
+          <option className="dropdown-options" value="Graceful">Graceful</option>
+          <option className="dropdown-options" value="Filthy">Filthy</option>
+          <option className="dropdown-options" value="Cloud">Cloud</option>
+          <option className="dropdown-options" value="Deep">Deep</option>
+          <option className="dropdown-options" value="Furious">Furious</option>
+          <option className="dropdown-options" value="Trail">Trail</option>
+          <option className="dropdown-options" value="Juicy">Juicy</option>
+          <option className="dropdown-options" value="Blind">Blind</option>
+          <option className="dropdown-options" value="Ship">Ship</option>
+          <option className="dropdown-options" value="Squeak">Squeak</option>
+          <option className="dropdown-options" value="Climb">Climb</option>
+          <option className="dropdown-options" value="Fall">Fall</option>
+          <option className="dropdown-options" value="United">United</option>
+          <option className="dropdown-options" value="Found">Found</option>
+          <option className="dropdown-options" value="Mask">Mask</option>
         </select>
         </div>
     )
@@ -82,39 +83,40 @@ class ArtworkList extends Component {
           name="artworkDate"
           value={this.props.artworkDate}
           onChange={this.props.handleInputChange}
+          className="dropdown-select"
         >
-          <option defaultValue="" value="">All Dates</option>
-          <option value="October 1, 2017">October 1, 2017</option>
-          <option value="October 2, 2017">October 2, 2017</option>
-          <option value="October 3, 2017">October 3, 2017</option>
-          <option value="October 4, 2017">October 4, 2017</option>
-          <option value="October 5, 2017">October 5, 2017</option>
-          <option value="October 6, 2017">October 6, 2017</option>
-          <option value="October 7, 2017">October 7, 2017</option>
-          <option value="October 8, 2017">October 8, 2017</option>
-          <option value="October 9, 2017">October 9, 2017</option>
-          <option value="October 10, 2017">October 10, 2017</option>
-          <option value="October 11, 2017">October 11, 2017</option>
-          <option value="October 12, 2017">October 12, 2017</option>
-          <option value="October 13, 2017">October 13, 2017</option>
-          <option value="October 14, 2017">October 14, 2017</option>
-          <option value="October 15, 2017">October 15, 2017</option>
-          <option value="October 16, 2017">October 16, 2017</option>
-          <option value="October 17, 2017">October 17, 2017</option>
-          <option value="October 18, 2017">October 18, 2017</option>
-          <option value="October 19, 2017">October 19, 2017</option>
-          <option value="October 20, 2017">October 20, 2017</option>
-          <option value="October 21, 2017">October 21, 2017</option>
-          <option value="October 22, 2017">October 22, 2017</option>
-          <option value="October 23, 2017">October 23, 2017</option>
-          <option value="October 24, 2017">October 24, 2017</option>
-          <option value="October 25, 2017">October 25, 2017</option>
-          <option value="October 26, 2017">October 26, 2017</option>
-          <option value="October 27, 2017">October 27, 2017</option>
-          <option value="October 28, 2017">October 28, 2017</option>
-          <option value="October 29, 2017">October 29, 2017</option>
-          <option value="October 30, 2017">October 30, 2017</option>
-          <option value="October 31, 2017">October 31, 2017</option>
+          <option className="dropdown-options" defaultValue="" value="">All Dates</option>
+          <option className="dropdown-options" value="October 1, 2017">October 1, 2017</option>
+          <option className="dropdown-options" value="October 2, 2017">October 2, 2017</option>
+          <option className="dropdown-options" value="October 3, 2017">October 3, 2017</option>
+          <option className="dropdown-options" value="October 4, 2017">October 4, 2017</option>
+          <option className="dropdown-options" value="October 5, 2017">October 5, 2017</option>
+          <option className="dropdown-options" value="October 6, 2017">October 6, 2017</option>
+          <option className="dropdown-options" value="October 7, 2017">October 7, 2017</option>
+          <option className="dropdown-options" value="October 8, 2017">October 8, 2017</option>
+          <option className="dropdown-options" value="October 9, 2017">October 9, 2017</option>
+          <option className="dropdown-options" value="October 10, 2017">October 10, 2017</option>
+          <option className="dropdown-options" value="October 11, 2017">October 11, 2017</option>
+          <option className="dropdown-options" value="October 12, 2017">October 12, 2017</option>
+          <option className="dropdown-options" value="October 13, 2017">October 13, 2017</option>
+          <option className="dropdown-options" value="October 14, 2017">October 14, 2017</option>
+          <option className="dropdown-options" value="October 15, 2017">October 15, 2017</option>
+          <option className="dropdown-options" value="October 16, 2017">October 16, 2017</option>
+          <option className="dropdown-options" value="October 17, 2017">October 17, 2017</option>
+          <option className="dropdown-options" value="October 18, 2017">October 18, 2017</option>
+          <option className="dropdown-options" value="October 19, 2017">October 19, 2017</option>
+          <option className="dropdown-options" value="October 20, 2017">October 20, 2017</option>
+          <option className="dropdown-options" value="October 21, 2017">October 21, 2017</option>
+          <option className="dropdown-options" value="October 22, 2017">October 22, 2017</option>
+          <option className="dropdown-options" value="October 23, 2017">October 23, 2017</option>
+          <option className="dropdown-options" value="October 24, 2017">October 24, 2017</option>
+          <option className="dropdown-options" value="October 25, 2017">October 25, 2017</option>
+          <option className="dropdown-options" value="October 26, 2017">October 26, 2017</option>
+          <option className="dropdown-options" value="October 27, 2017">October 27, 2017</option>
+          <option className="dropdown-options" value="October 28, 2017">October 28, 2017</option>
+          <option className="dropdown-options" value="October 29, 2017">October 29, 2017</option>
+          <option className="dropdown-options" value="October 30, 2017">October 30, 2017</option>
+          <option className="dropdown-options" value="October 31, 2017">October 31, 2017</option>
         </select>
         </div>
     )
@@ -124,10 +126,13 @@ class ArtworkList extends Component {
   render() {
     return (
       <div className="artworklist">
-        {this.filterArtworkDates()}
-        {this.filterArtworkPrompts()}
+        <div className="dropdowns">
+          {this.filterArtworkDates()}
+          {this.filterArtworkPrompts()}
+        </div>
+        
         {(this.state.artworkDataLoaded) ? 
-        <div>
+          <div>
         {this.state.artworkData
           .filter((artwork) => {
             if(this.props.artworkPrompt === '' || this.props.artworkDate === '')
@@ -137,7 +142,7 @@ class ArtworkList extends Component {
           })
           .map((artwork) => {
             console.log(artwork)
-            return <ArtworkSingle artwork={artwork} key={artwork.id} />
+            return <ArtworkSingle artwork={artwork} key={artwork.id} handleComments={this.props.handleComments} />
           })}
       </div>
        : <p>Loading...</p>}
