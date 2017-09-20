da37c9550ede9532feb5693238e3edf669eab8e0
Alexander Calleia <alex_calleia@yahoo.com>
2017-09-20 01:48:56 -0400

    add comments table migration

---
+++ db/migrate/20170918194816_create_comments.rb
@@ -0,0 +1,14 @@
+class CreateComments < ActiveRecord::Migration[5.1]
+  def change
+    create_table :comments do |t|
+      t.integer :user_id
+      t.integer :artwork_id
+      t.text :comment
+
+      t.timestamps
+    end
+
+    add_index :comments, :user_id
+    add_index :comments, :artwork_id
+  end
+end
